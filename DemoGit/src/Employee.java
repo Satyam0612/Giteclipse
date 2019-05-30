f3539e16864b801a10f3c9482d17f4275b828d85
satyam0612 <er.satyamshrivastava@gmail.com>
2019-05-30 04:14:34 -0700

    Employee Mode1

---
+++ DemoGit/src/Employee.java
@@ -0,0 +1,25 @@
+
+public class Employee {
+  private int id;
+  private String name;
+  private double empSal;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public double getEmpSal() {
+	return empSal;
+}
+public void setEmpSal(double empSal) {
+	this.empSal = empSal;
+}
+  
+}
